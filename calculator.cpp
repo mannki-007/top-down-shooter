15c301d8b397e43297469abbdaf86edba59fb60c
mannki-007 <dayzerpc12@gmail.com>
2025-11-05 21:52:28 +0300

    A simple calculator on cpp
    nothing

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "num 1" << endl;
+		cin >> a;
+	int b;
+	cout << "num 2" << endl;
+	    cin >> b;
+		char c;
+		cout << "+|-" << endl;
+		cin >> c;
+		if(c == '+')
+		{
+			cout << (a + b);
+		}
+
+		if(c == '-')
+		{
+			cout << (a - b);
+		}
+
+		
+	
+}
